728e88b34d34a9cc87b67feeda0f98f5b01858a0
Zuzanna Hasiak <zuzanka@laptop-Karolina.local>
2025-11-07 16:34:18 +0100

    Add H3_main

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -3,7 +3,26 @@ package h3;
 public class H3_main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int max = 80;
+		int fix = 40;
+		int wartend = 30;
+		boolean istVoll = false;
+		
+		
+		while (fix < max && wartend > 0) {
+			fix = fix + 1;
+			wartend = wartend - 1;
+		}
+		
+		if (fix == max) {
+			istVoll = true;
+		} else {
+			istVoll = false;
+		}
+		
+		System.out.println("Fixplatze vergeben: " + fix);
+		System.out.println("Wartende ubrig: " + wartend);
+		System.out.println("Ist voll: " + istVoll);
 
 	}
 
