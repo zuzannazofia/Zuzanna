ebdd3565f105ee0cb083763942fdb325e088a30c
Zuzanna Hasiak <zuzanka@laptop-Karolina.local>
2025-11-07 16:21:02 +0100

    Add H2_main

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -3,7 +3,18 @@ package h2;
 public class H2_main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int Jahr = 2016;
+		boolean schalt = false;
+		
+		if ((Jahr % 4 ==0 && Jahr % 100 !=0) || (Jahr % 400 == 0)) {
+			schalt = true;
+		} else {
+			schalt = false;
+		}
+		
+		System.out.println("Jahr: " + Jahr);
+		System.out.println("Schaltjahr: " + schalt);
+		
 
 	}
 
