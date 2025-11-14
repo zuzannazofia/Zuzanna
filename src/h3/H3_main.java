463e2015c3cdf6ba281136dc02c0c7b33efff250
Zuzanna Hasiak <zuzanka@laptop-Karolina.fritz.box>
2025-11-14 20:06:54 +0100

    Add H3_main

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -2,4 +2,19 @@ package h3;
 
 public class H3_main {
 
+	public static void main(String[] args) {
+		float celsiusFloat = 30;
+		float fahrenheitFloat ; 
+		double celsiusdouble = 5;
+		double fahrenheitdouble ;
+		
+		fahrenheitFloat = celsiusFloat * 9/5 + 32;
+		fahrenheitdouble = celsiusdouble * 9/5 + 32;
+		
+		System.out.println("Float Fahrenheit: " + fahrenheitFloat);
+		System.out.println("Double Fahrenheit: " + fahrenheitdouble);
+		
+
+	}
+
 }
